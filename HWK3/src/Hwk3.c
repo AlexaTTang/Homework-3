1843b874c5e16a1d6b53cd5b28a3a1bdb2d7a05a
Alexa Tang <alexatang@cpp.edu>
2019-02-18 21:53:22 -0800

    Fourth commit

--- HWK3/src/Hwk3.c
+++ HWK3/src/Hwk3.c
@@ -20,63 +20,78 @@ int main()
 	printf("Enter your choice (1-4): \n");
 	//reads the input and stores it at the address of variable option
 	scanf("%d", &option);
-
+		//depending on what the value of option is determines which case is executed
 		switch(option)
 		{
+			//calculates the area of a circle
 			case 1:
+				//asking for user input
 				printf("Enter the radius of the circle: \n");
 				scanf("%d", &radius);
+					//checks to see if input is a negative number
 					if (radius<0)
 					{
 						printf("Error, cannot have negative values");
 					}
+					//if input is not a negative number then area is calculated and displayed the on console
 					else
 					{
-					area=(radius*radius)*3.14;
+					area=(radius*radius)*3.14159;
 					printf("The area of the circle is: %d", area);
 					}
+				//ends case 1
 				break;
-
+			//calculates the area of a rectangle
 			case 2:
+				//asking for user input
 				printf("Enter the length of the rectangle: \n");
 				scanf("%d", &length);
 				printf("Enter the width of the rectangle: \n");
 				scanf("%d", &width);
-					if (length< 0 || width < 0)
+					//checks to see if either length or width are negative values
+					if (length < 0 || width < 0)
 					{
 						printf("Error, cannot have negative values");
 					}
+					//if they are not negative then the area is calculated and displayed on the console
 					else
 					{
 						area = length * width;
 						printf("The area of the rectangle is: %d", area);
 					}
+				//ends case 2
 				break;
-
+			//calculates the area of a triangle
 			case 3:
+				//asking for user input
 				printf("Enter the base of the triangle: \n");
 				scanf("%d", &base);
 				printf("Enter the height of the triangle: \n");
 				scanf("%d", &height);
+					//checks to see if either base or height are negative values
 					if (base < 0 || height < 0)
 					{
 						printf("Error, cannot have negative values");
 					}
+					//if they are not negative then the area is calculated and displayed on the console
 					else
 					{
 						area = base * height * 0.5;
 						printf("The area of the triangle is: %d", area);
 					}
+				//ends case 3
 				break;
-
+			//if user wants to quit
 			case 4:
 				printf("You should use a calculator instead");
 				break;
-
+			// this is executed if the user enters a number that is not between 1-4
 		    default:
 				printf("Please enter a number between 1-4");
 				break;
 		}
+
+		//Population
 
 
 
