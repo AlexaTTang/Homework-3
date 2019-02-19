4d959079811a30b9c77e83066d8e34a6311b46ec
Alexa Tang <alexatang@cpp.edu>
2019-02-18 16:59:51 -0800

    First commit

---
+++ HWK3/src/Hwk3.c
@@ -0,0 +1,25 @@
+/*
+ * Hwk3.c
+ *
+ *  Created on: Feb 18, 2019
+ *      Author: alexa
+ */
+#include <stdio.h>
+int main()
+{
+
+	//Geometry Calculator
+	printf("Geometry Calculator\n\n");
+	printf("\t 1. Caluculate the Area of a Circle\n");
+	printf("\t 2. Caluculate the Area of a Rectangle\n");
+	printf("\t 3. Caluculate the Area of a Triangle\n");
+	printf("\t 4. Quit\n\n");
+	printf("Enter your choice (1-4): \n");
+
+
+
+	return 0;
+}
+
+
+
