c39651d2a998bfcc2051f03f22ee4190721d15e4
Alexa Tang <alexatang@cpp.edu>
2019-02-18 21:08:24 -0800

    Second commit

--- HWK3/src/Hwk3.c
+++ HWK3/src/Hwk3.c
@@ -1,3 +1,4 @@
+
 /*
  * Hwk3.c
  *
@@ -9,17 +10,38 @@ int main()
 {
 
 	//Geometry Calculator
+	int  option, radius, length, width, base, height, area, pi;
+	//Menu
 	printf("Geometry Calculator\n\n");
-	printf("\t 1. Caluculate the Area of a Circle\n");
-	printf("\t 2. Caluculate the Area of a Rectangle\n");
-	printf("\t 3. Caluculate the Area of a Triangle\n");
+	printf("\t 1. Calculate the Area of a Circle\n");
+	printf("\t 2. Calculate the Area of a Rectangle\n");
+	printf("\t 3. Calculate the Area of a Triangle\n");
 	printf("\t 4. Quit\n\n");
 	printf("Enter your choice (1-4): \n");
+	//reads the input and stores it at the address of variable option
+	scanf("%d", &option);
+
+	//printf("you entered: %d", option);
+		switch(option)
+		{
+			case 1:
+
+			printf("Enter the radius of the circle: \n");
+			scanf("%d", &radius);
+			area=(radius*radius)*3.14;
+			printf("The area of the circle is: %d", area);
+			break;
+
+			  default:
+			  printf("hello");
+			  break;
+
+
+		}
 
 
 
 	return 0;
 }
-
 
 
